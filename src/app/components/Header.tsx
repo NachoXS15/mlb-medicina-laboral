fb898bea05456d0691123165783b0ad25423b4f5
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-07-07 17:17:14 -0300

    start project

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,10 @@
+export default async function Header() {
+  return (
+    <header>
+        <a href="">asdasdasdasdad</a>
+        <nav>
+            <a href=""></a><a href=""></a><a href=""></a>
+        </nav>
+    </header>
+  )
+}
