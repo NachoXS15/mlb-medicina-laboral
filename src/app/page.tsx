d094e4ec4bba0fd78c776d7cc2796bfdb63d85ab
LucasP86 <lucasp8696@gmail.com>
2025-07-16 15:48:49 -0300

    16-JULY | Lucas - Little changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,39 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
 		<>
-			<h2 className="text-red-200 underline">ACA TRABAJAR</h2>
-			<a href="/services">ir a servicios</a>
+			<section id="banner" className="bg-blue-900">
+				<div className="w-full min-h-100 bg-blue-300 flex flex-col items-start justify-center text-gray-950 p-10 lg:max-w-5xl lg:mx-auto lg:py-2">
+					<div id="banner-description">
+						<h2 className="text-xs">
+							Dra. Basso, medicina laboral para empresas en La
+							Rioja
+						</h2>
+						<h1 className="font-bold text-2xl mb-3">
+							Medicina laboral <br /> para empresas
+						</h1>
+						<Link
+							href="/"
+							className="px-4 py-2 bg-sky-800 font-bold text-white rounded-full"
+						>
+							{" "}
+							Contactanos{" "}
+						</Link>
+					</div>
+				</div>
+			</section>
+			<main className="w-full min-h-100 dark:bg-sky-950 dark:text-white flex flex-col items-start justify-center text-gray-950 p-10 lg:max-w-5xl lg:mx-auto lg:py-2">
+				<section id="aboutUs">
+					{/* titulos */}
+					<div>
+						<h3>Sobre nosotros</h3>
+					</div>
+				</section>
+				<section id="clients">
+					<h3>Nuestros clientes</h3>
+				</section>
+			</main>
 		</>
 	);
 }
