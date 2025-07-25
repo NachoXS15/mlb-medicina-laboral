8011ddf16909b0b253dc329904cce3995c623f53
StJemmy <kerplunk37@gmail.com>
2025-07-25 19:10:41 -0300

    - adding mobile style dashboardU

--- src/app/admin/dashboardU/page.tsx
+++ src/app/admin/dashboardU/page.tsx
@@ -1,20 +1,34 @@
-import logo from '../../assets/logo_blanco.webp'
-import Image from 'next/image'
+import logo from "../../assets/logo_blanco.webp";
+import Image from "next/image";
 
 export default function page() {
-  return (
-    <>  
-        <main className="w-full min-h-screen bg-cover bg-center " style={{backgroundImage: `url('/assets/ypf.jpg')`}}>
-            <div className="w-full min-h-screen bg-s-shadow/50 flex items-center justify-center flex-col gap-5">
-                <Image src={logo} width={150} className="absolute left-40 top-40" alt="" /> 
-                <h2 className="text-white font-main text-6xl font-bold">¡Bienvenido, YPF!</h2>
-                <a href="#docs" className="text-2xl text-white font-main">Ver mis documentos</a>
-            </div>
-        </main>
-        {/* <section className="w-full min-h-screen" id="docs">
+	return (
+		<>
+			<main
+				className="w-full min-h-screen bg-cover bg-center "
+				style={{ backgroundImage: `url('/assets/ypf.jpg')` }}
+			>
+				<div className="w-full min-h-screen bg-radial from-s-shadow/50 to-s-shadow flex items-center justify-center flex-col gap-5">
+					<Image
+						src={logo}
+						width={150}
+						className="md:absolute left-20 top-20"
+						alt=""
+					/>
+					<h2 className="text-white font-main text-xl md:text-6xl font-bold">
+						¡Bienvenido, YPF!
+					</h2>
+					<a
+						href="#docs"
+						className="text-base md:text-2xl text-white font-main"
+					>
+						Ver mis documentos
+					</a>
+				</div>
+			</main>
+			{/* <section className="w-full min-h-screen" id="docs">
             <h2>Mis documentos</h2>
         </section> */}
-        
-    </>
-  )
+		</>
+	);
 }
