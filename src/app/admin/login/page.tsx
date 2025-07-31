b099719915622870c92b82e2a7c43b93e59d56c3
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-07-31 20:50:37 -0300

    trying to create roles

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
 import logo from '../../assets/logo.webp'
-import Link from 'next/link'
-export default function page() {
+import { login } from './actions'
+// import { createClient } from '@/app/utils/supabase/server'
+// import { redirect } from 'next/navigation'
+
+export default async function page() {
+
+    // const supabase = await createClient()
+    // const { data } = await supabase.auth.getUser();
+
+    // const userName = data.user?.user_metadata
+    // console.log(userName);
+    
+    // if(!error || data?.user){
+    //     redirect("/dashboard")
+    // }
+
+
   return (
     <main className='bg-white w-full min-h-screen flex items-center justify-center font-main'>
         <section className='text-black w-[650px] h-[550px] shadow-xl'>
@@ -15,13 +30,13 @@ export default function page() {
                 <form action="" className='w-full flex items-center flex-col gap-5'>
                     <div className='w-4/5 flex flex-col'>
                         <label htmlFor="">Email</label>
-                        <input type="text" className='h-10 border border-bluemain rounded-2xl' />
+                        <input type="email" name='email' className='h-10 px-5 border border-bluemain rounded-2xl' />
                     </div>
                     <div className='w-4/5 flex flex-col'>
                         <label htmlFor="">Contraseña</label>
-                        <input type="password" className='h-10 border border-bluemain rounded-2xl' />
+                        <input type="password" name='password' className='h-10 px-5 border border-bluemain rounded-2xl' />
                     </div>
-                    <Link href="/admin/dashboardU" className='mt-5 bg-bluemain flex justify-center items-center text-white w-4/5 h-10 rounded-2xl transition border hover:border-bluemain hover:bg-white hover:text-bronze'>Iniciar sesión</Link>
+                    <button formAction={login} className='mt-5 bg-bluemain flex justify-center items-center text-white w-4/5 h-10 rounded-2xl transition border hover:border-bluemain hover:bg-white hover:text-bronze'>Iniciar sesión</button>
                 </form>
                 <div>
                     <p className='text-bluemain text-md font-bold hover:underline'>¿Olvidaste tu contraseña?</p>
