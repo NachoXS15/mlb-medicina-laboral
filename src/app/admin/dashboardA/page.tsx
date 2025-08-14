eb80969fbaf531137f0934f0cd785c8cc3135438
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-08-14 18:18:12 -0300

    created search form in admin

--- src/app/admin/dashboardA/page.tsx
+++ src/app/admin/dashboardA/page.tsx
@@ -1,8 +1,7 @@
+import SearchFormAdmin from "@/app/components/SearchFormAdmin";
 import { ProfileType } from "@/app/config/definitions";
 import { fetchProfiles } from "@/app/lib/data-server";
 import { createClient } from "@/app/utils/supabase/server";
-import TableA from "@/app/utils/tables/TableA";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 export default async function page() {
 
@@ -15,25 +14,13 @@ export default async function page() {
     }else if(profile?.role == "client"){
         redirect('/admin/dashboardU')
     }
-
+    const profileName = profile.name;
     const profiles: ProfileType[] = (await fetchProfiles()) ?? [];
 
     return (
         <>
             <main className="flex flex-col w-full font-main my-15 text-gray-700 bg-white justify-center items-center ">
-                <div className="w-10/12">
-                    <div className="flex justify-between mb-5">
-                        <h2 className="text-xl mb-3">Hola, {profile.name}</h2>
-                        <form action="" className="flex gap-3 items-center">
-                            <input type="text" className="h-9 w-44 px-2 border rounded border-slate-300" placeholder="Cliente" />
-                            <button type="submit" className=" bg-bronze text-slate-100 px-5 py-1 rounded">Buscar</button>
-                        </form>
-                        <Link href="/admin/dashboardA/addClient" className="text-xl underline">Agregar Cliente o Usuario</Link>
-                    </div>
-                    <div className="h-[700px] overflow-y-scroll">
-                        <TableA profiles={profiles} />
-                    </div>
-                </div>
+                <SearchFormAdmin profiles={profiles} profile={profileName} />
             </main>
         </>
     )
