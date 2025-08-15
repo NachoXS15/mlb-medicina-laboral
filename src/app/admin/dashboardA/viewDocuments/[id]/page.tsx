761b6d37af3cbd42b2babcd0c1ae2539684a7a55
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-08-15 01:51:20 -0300

    finished addDocument form

--- src/app/admin/dashboardA/viewDocuments/[id]/page.tsx
+++ src/app/admin/dashboardA/viewDocuments/[id]/page.tsx
@@ -7,24 +7,36 @@ export default async function page({ params }: InferGetServerSidePropsType<typeo
     const { id } = await params;
     const profile = await fetchProfilebyId(id)
 
+    const
+
+    const docs: string[] = [];
+    const 
+
+
     return (
         <>
-            <section className="w-10/12 m-auto min-h-screen flex items-center my-10 justify-center flex-col gap-3 font-main" id="docs">
+            <section className="w-10/12 m-auto min-h-screen flex items-center justify-center flex-col gap-3 font-main" id="docs">
                 <div className="w-full flex justify-between items-center">
                     <h2 className='self-start font-main'>Documentos de: <span className="font-bold">{profile?.name}</span></h2>
                     <Link href={`/admin/dashboardA/viewDocuments/${id}/addDocument`} className="font-bold hover:underline">Agregar Documento</Link>
                 </div>
                 <section className='w-full h-[700px] border border-slate-300'>
-                    <div className='flex items-center justify-between bg-slate-100 border-b border-b-slate-400 p-5'>
-                        <div>
-                            <h2 className='font-bold'>Julio_2025</h2>
-                            <span>Subido: </span>
-                        </div>
-                        <div className="flex items-center gap-5">
-                            <Link href="" className='hove:scale-105 transition'><Download size={24} color='#000' /></Link>
-                            <Link href="" className="hover:scale-105 transition"><Trash size={24} /></Link>
-                        </div>
-                    </div>
+                    {docs.length == 0 ? (
+                        <div className='flex items-center justify-center w-full h-full'>
+                            <h2>No hay documentos cargados.</h2>
+                        </div> 
+                    ): docs.map((doc, i) => (
+                        <div key={i} className='flex items-center justify-between bg-slate-100 border-b border-b-slate-400 p-5'>
+                            <div>
+                                <h2 className='font-bold'>Julio_2025</h2>
+                                <span>Subido: </span>
+                            </div>
+                            <div className="flex items-center gap-5">
+                                <Link href="" className='hove:scale-105 transition'><Download size={24} color='#000' /></Link>
+                                <Link href="" className="hover:scale-105 transition"><Trash size={24} /></Link>
+                            </div>
+                        </div> 
+                    ))}
                 </section>
             </section>
         </>
