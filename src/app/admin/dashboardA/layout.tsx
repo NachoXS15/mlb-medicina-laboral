30f14c34aa1961c7f1b046882b91e23866bb600b
NachoXS-NET <pantojaignacio642@gmail.com>
2025-08-20 22:41:32 -0300

    fixing bugs

--- src/app/admin/dashboardA/layout.tsx
+++ src/app/admin/dashboardA/layout.tsx
@@ -1,5 +1,4 @@
 import HeaderA from "@/app/components/HeaderA";
-import { ProfileType } from "@/app/config/definitions";
 import { createClient } from "@/app/utils/supabase/server";
 import { redirect } from "next/navigation";
 
@@ -8,9 +7,6 @@ export default async function layout({ children }: { children: React.ReactNode }
   const supabase = await createClient()
   const { data, error } = await supabase.auth.getUser()
   const { data: profile } = await supabase.from('profiles').select('*').eq('id', data.user?.id).single()
-
-  console.log(profile);
-  
 
   if (error || !data?.user || profile.status == "Inactivo") {
     redirect('/admin/login')
