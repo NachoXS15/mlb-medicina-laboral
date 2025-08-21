6722d3b8047e7ccba3290424644ad3b973491a04
LucasP86 <lucasp8696@gmail.com>
2025-08-20 22:44:16 -0300

    20-August | #Lucas - Adding mobile version

--- src/app/admin/dashboardA/page.tsx
+++ src/app/admin/dashboardA/page.tsx
@@ -4,24 +4,27 @@ import { fetchProfiles } from "@/app/lib/data-server";
 import { createClient } from "@/app/utils/supabase/server";
 import { redirect } from "next/navigation";
 export default async function page() {
+	const supabase = await createClient();
+	const { data, error } = await supabase.auth.getUser();
+	const { data: profile } = await supabase
+		.from("profiles")
+		.select("*")
+		.eq("id", data.user?.id)
+		.single();
 
-    const supabase = await createClient()
-    const { data, error } = await supabase.auth.getUser()
-    const { data: profile } = await supabase.from('profiles').select('*').eq('id', data.user?.id).single()
+	if (error || !data?.user) {
+		redirect("/admin/login");
+	} else if (profile?.role == "client") {
+		redirect("/admin/dashboardU");
+	}
+	const profileName = profile.name;
+	const profiles: ProfileType[] = (await fetchProfiles()) ?? [];
 
-    if (error || !data?.user) {
-        redirect('/admin/login')
-    }else if(profile?.role == "client"){
-        redirect('/admin/dashboardU')
-    }
-    const profileName = profile.name;
-    const profiles: ProfileType[] = (await fetchProfiles()) ?? [];
-
-    return (
-        <>
-            <main className="flex flex-col w-full font-main my-15 text-gray-700 bg-white justify-center items-center ">
-                <SearchFormAdmin profiles={profiles} profile={profileName} />
-            </main>
-        </>
-    )
+	return (
+		<>
+			<main className="flex flex-col w-full font-main my-5 md:my-15 text-gray-700 bg-white justify-center items-center ">
+				<SearchFormAdmin profiles={profiles} profile={profileName} />
+			</main>
+		</>
+	);
 }
--- src/app/components/SearchFormAdmin.tsx
+++ src/app/components/SearchFormAdmin.tsx
@@ -1,36 +1,55 @@
-'use client'
-import Link from "next/link"
-import TableA from "../utils/tables/TableA"
+"use client";
+import Link from "next/link";
+import TableA from "../utils/tables/TableA";
 import { useMemo, useState } from "react";
-import { ProfileType } from "../config/definitions"
+import { ProfileType } from "../config/definitions";
 
 type Props = {
-    profiles: ProfileType[]
-    profile: string
-}
+	profiles: ProfileType[];
+	profile: string;
+};
 
 export default function SearchFormAdmin({ profiles, profile }: Props) {
+	const [query, setQuery] = useState("");
 
-    const [query, setQuery] = useState("");
+	const filteredProfiles = useMemo(() => {
+		return profiles.filter((profile) =>
+			profile.name.toLowerCase().includes(query.toLowerCase())
+		);
+	}, [query, profiles]);
 
-    const filteredProfiles = useMemo(() => {
-        return profiles.filter(profile => profile.name.toLowerCase().includes(query.toLowerCase()));
-    }, [query, profiles])
-
-
-    return (
-        <div className="w-10/12">
-            <div className="flex justify-between mb-5">
-                <h2 className="text-xl mb-3">Hola, {profile}</h2>
-                <form onSubmit={(e) => e.preventDefault()} className="flex gap-3 items-center">
-                    <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className="h-9 w-44 px-2 border rounded border-slate-300" placeholder="Cliente" />
-                    <button type="submit" className=" bg-bronze text-slate-100 px-5 py-1 rounded">Buscar</button>
-                </form>
-                <Link href="/admin/dashboardA/addClient" className="text-xl underline">Agregar Cliente o Usuario</Link>
-            </div>
-            <div className="h-[700px] overflow-y-scroll">
-                <TableA profiles={filteredProfiles} />
-            </div>
-        </div>
-    )
+	return (
+		<div className="w-10/12 bg-red-300">
+			<div className="flex flex-col md:flex-row justify-between mb-5">
+				<h2 className="text-xl mb-3">Hola, {profile}</h2>
+				<form
+					onSubmit={(e) => e.preventDefault()}
+					className="flex gap-3 items-center"
+				>
+					<input
+						type="text"
+						value={query}
+						onChange={(e) => setQuery(e.target.value)}
+						className="h-9 w-44 px-2 border rounded border-slate-300"
+						placeholder="Cliente"
+					/>
+					<button
+						type="submit"
+						className=" bg-bronze text-slate-100 px-5 py-1 rounded"
+					>
+						Buscar
+					</button>
+				</form>
+				<Link
+					href="/admin/dashboardA/addClient"
+					className="text-xl underline"
+				>
+					Agregar Cliente o Usuario
+				</Link>
+			</div>
+			<div className="h-[700px] overflow-y-scroll">
+				<TableA profiles={filteredProfiles} />
+			</div>
+		</div>
+	);
 }
